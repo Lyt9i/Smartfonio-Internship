ca8ee8ae2af9a7fb738b6e39972d9714f289a265
Lyt9i <afadkag@gmail.com>
2026-07-19 07:19:32 +0300

    Update CubeMovement.cs
    Проверка работспособности гит

--- My project/Assets/Scripts/CubeMovement.cs
+++ My project/Assets/Scripts/CubeMovement.cs
@@ -7,7 +7,7 @@ public class CubeMovement : MonoBehaviour
     private bool isMoving = false;
     private bool isFirstPress = true;
     private Animator modelAnimator;
-
+    //авы
     void Start()
     {
         modelAnimator = GetComponentInChildren<Animator>();
